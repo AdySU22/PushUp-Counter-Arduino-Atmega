4c0307afd8e799c97eb36583b26ea6135b427cd6
AdySU22 <ady.business17@gmail.com>
2025-04-10 14:37:00 +0700

    relocated files to new folder to make room for server code

---
+++ PushUp-Counter-Arduino-Atmega/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <avr/io.h>
+#include "led.h"
+#include "power&reset_switch.h"
+// #include "timer.h"
+#include "server.h"
+
+#include "lcd.h"
+
+typedef enum
+{
+
+} SwitchState;
+
+
+int main()
+{
+    
+
+}
+
+ISR(PCINT0_vect)
+{
+    
+}
--- PushUp-Counter-Arduino-Atmega/src/reset_switch.cpp
+++ PushUp-Counter-Arduino-Atmega/src/reset_switch.cpp
@@ -1,6 +1,7 @@
 #include "switch.h"
 #include <avr/io.h>
 
-void init_switch_pin(void) {
+void initSwitch()
+{
     
 }
