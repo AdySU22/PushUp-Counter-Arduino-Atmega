adb2bc3f58fee418f3545d3703d1801ce5919958
AdySU22 <ady.business17@gmail.com>
2025-04-12 19:56:03 +0700

    define DDRs in common.h

--- PushUp-Counter-Arduino-Atmega/src/common.h
+++ PushUp-Counter-Arduino-Atmega/src/common.h
@@ -2,3 +2,16 @@
 #define COMMON_H__
 #include <avr/io.h>
 #define TODO() static_assert(false, "TODO: implementation needed in " __FILE__);
+
+// DDRs
+#ifndef DDRS__
+#define DDRS__
+
+#define INFRARED_SENSOR_DDR DDRK
+#define RESET_SWITCH_DDR DDRJ
+#define LEFT_TOUCH_SENSOR_DDR DDRB
+#define RIGHT_TOUCH_SENSOR_DDR DDRB
+#define SPI_DDR DDRB
+#define I2C_DDR DDRD
+
+#endif // DDRS__
